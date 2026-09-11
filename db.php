fb37f5ba574199965f1607478dac82e506a8cad0
cajuraoriza5-png <cajuraoriza5@gmail.com>
2026-09-12 01:59:55 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "attendance");
+$conn = new mysqli("sql107.infinityfree.com", "if0_42609958", "Arriannah100924", "if0_42609958_attendance", 3306);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
