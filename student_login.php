866df0bdbbdcc53d057800af1a7bcdac4debb452
cajuraoriza5-png <cajuraoriza5@gmail.com>
2026-09-12 00:53:03 +0800

    Update student_login.php

--- student_login.php
+++ student_login.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 $conn = new mysqli("sql107.infinityfree.com", "if0_42609958", "Arriannah100924", "if0_42609958_attendance", 3306);");
 
